97c3488d823269c4a5708a097b2615e47f5bceb8
Rameshwor Dhakal <dhakalramu2070@gmail.com>
2020-01-31 23:15:41 +0545

    added two milestones, qualify and final game

--- lib/ScoreUpdateScreen.dart
+++ lib/ScoreUpdateScreen.dart
@@ -12,14 +12,23 @@ class _ScoreUpdateScreenState extends State<ScoreUpdateScreen> {
   gotonewscreen(BuildContext context, index) async {
     var finalScore = await Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => new ScoreUpdateDialog(scores[index])),
+      MaterialPageRoute(
+          builder: (context) => new ScoreUpdateDialog(scores[index])),
     );
     setState(() {
       scores[index] = finalScore;
     });
   }
 
-    Future<bool> _onBackPressed() {
+  returnMileStoneScore(int score) {
+    if (score <= 83) {
+      return (83 - score);
+    } else {
+      return (100 - score);
+    }
+  }
+
+  Future<bool> _onBackPressed() {
     return showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -46,61 +55,64 @@ class _ScoreUpdateScreenState extends State<ScoreUpdateScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(onWillPop: _onBackPressed, child: Scaffold(
-      appBar: AppBar(
-        title: Text("Score Update"),
-      ),
-      body: Container(
-        child: GridView.builder(
-          itemCount: widget.players.length,
-          itemBuilder: (context, index) {
-            return Card(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.all(6),
-                    alignment: Alignment.center,
-                    child: Text(
-                      widget.players[index],
-                      style: TextStyle(fontSize: 20.0),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 20.0),
-                    alignment: Alignment.center,
-                    child: Text(
-                      scores[index].toString(),
-                      style: TextStyle(fontSize: 45.0),
-                    ),
-                  ),
-                  Container(
-                    child: RaisedButton(
-                      child: Text(
-                        'Add Score',
-                        style: TextStyle(color: Colors.white, fontSize: 20.0),
-                      ),
-                      onPressed: () => this.gotonewscreen(context, index),
-                      color: Colors.blue,
-                    ),
-                    margin: EdgeInsets.only(top: 20.0),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 5),
-                    alignment: Alignment.center,
-                    child: Text(
-                      (83-scores[index]).toString(),
-                      style: TextStyle(fontSize: 30.0),
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
+    return WillPopScope(
+        onWillPop: _onBackPressed,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Score Update"),
           ),
-        ),
-      ),
-    ));
+          body: Container(
+            child: GridView.builder(
+              itemCount: widget.players.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.all(6),
+                        alignment: Alignment.center,
+                        child: Text(
+                          widget.players[index],
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20.0),
+                        alignment: Alignment.center,
+                        child: Text(
+                          scores[index].toString(),
+                          style: TextStyle(fontSize: 45.0),
+                        ),
+                      ),
+                      Container(
+                        child: RaisedButton(
+                          child: Text(
+                            'Add Score',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 20.0),
+                          ),
+                          onPressed: () => this.gotonewscreen(context, index),
+                          color: Colors.blue,
+                        ),
+                        margin: EdgeInsets.only(top: 20.0),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 5),
+                        alignment: Alignment.center,
+                        child: Text(
+                          returnMileStoneScore(scores[index]).toString(),
+                          style: TextStyle(fontSize: 30.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+            ),
+          ),
+        ));
   }
 }
